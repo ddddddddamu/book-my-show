ca0d75789fc07a21ced618083668ac679655b1d7
ddddddddamu <123865802+ddddddddamu@users.noreply.github.com>
2023-01-29 13:10:02 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import './App.css';
+import "./App.css";
+
+// Pages
+import HomePage from "./pages/Home.page";
+import MoviePage from "./pages/Movie.page";
+import PlayPage from "./pages/Play.page";
+
+// Routes
+import {Routes, Route} from 'react-router-dom';
+import axios from "axios";
+
+// React slick css
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+axios.defaults.baseURL= "https://api.themoviedb.org/3";
+axios.defaults.params ={};
+// axios.defaults.params["api_key"]= process.env.REACT_APP_API_KEY;
+axios.defaults.params["api_key"]= "c54ab2f18bdf2fcbf4283c5aa5ab488c";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/movie/:id" element={<MoviePage />} />
+    <Route path="/plays" element={<PlayPage />} />
+  </Routes>
 }
 
 export default App;
